c4f9d0cc4bcd289edbca6914d7995c3745e5111a
Nestor Martinez Ostoa <nestor.martinez.98@hotmail.com>
2018-04-25 19:47:09 -0500

    resolviendo problemas de archivos

--- Segunda Semana/Martes/Tareas/Ejercicio2/js/app.js
+++
@@ -1,5 +0,0 @@
-function iniciar(){
-	var estilo = document.getElementById("textoEstilo").value;
-	document.getElementById("estiloParaAplicar").style.color = estilo;
-	document.getElementById("textoEstilo").value = "";
-}
--- Segunda Semana/Martes/Tareas/Ejercicio3/js/app.js
+++
@@ -1,8 +0,0 @@
-function cambiar(){
-	var texto = document.getElementById("textoInsertado").value;
-	var node = document.createElement("LI");
-	var textnode = document.createTextNode(texto);
-	node.appendChild(textnode);
-	document.getElementById("miUL").appendChild(node);
-	document.getElementById("textoInsertado").value = "";
-}
